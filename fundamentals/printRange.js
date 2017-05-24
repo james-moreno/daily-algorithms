01a9060f0ff1bc1fc5fd693f29ae556a90cfa7e1
james-moreno <bonsaimo7@gmail.com>
2017-05-24 17:17:31 -0600

    print range of numbers

---
+++ fundamentals/printRange.js
@@ -0,0 +1,12 @@
+// Print integers from -52 to 1066 using a for loop.
+
+var testMin = -52;
+var testMax = 1066;
+
+function printRange(min, max) {
+  for (var i = min; i <= max; i++) {
+    console.log(i);
+  }
+}
+
+printRange(testMin, testMax);
