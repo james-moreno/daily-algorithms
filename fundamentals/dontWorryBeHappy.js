815551a06ca594810eae1c35b0f30d68434e0d73
james-moreno <bonsaimo7@gmail.com>
2017-05-24 17:27:47 -0600

    don't worry be happy

---
+++ fundamentals/dontWorryBeHappy.js
@@ -0,0 +1,11 @@
+// Create a function that console.logs the string "good morning!" 98 times.
+
+function beCheerful() {
+  var times = 98;
+  while(times !== 0) {
+    console.log('good morning!');
+    times--;
+  }
+}
+
+beCheerful();
