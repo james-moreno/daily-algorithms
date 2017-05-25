50eb01a5a5cbb981d4ade0d6a17bdaa3308804f8
james-moreno <bonsaimo7@gmail.com>
2017-05-25 12:52:31 -0600

    You say it's your birthday

---
+++ fundamentals/youSayItsYourBirthday.js
@@ -0,0 +1,10 @@
+// If 2 given numbers represent your birth month and day in either order, log "How did you know?", else log "Just another day..."
+
+function isBirthday(day, month){
+  if(day == 5 && month == 8) {
+    console.log('How did you know?');
+  }
+  else {
+    console.log('Just another day...');
+  }
+}
