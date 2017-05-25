51ba166e5476866afbab7c358d480bb712e47908
james-moreno <bonsaimo7@gmail.com>
2017-05-25 12:29:44 -0600

    Printing integers withwhile

---
+++ fundamentals/integersWithWhile.js
@@ -0,0 +1,11 @@
+// Print integers from 2000 to 5280, using a while
+
+function integersWhile(){
+  var start = 2000;
+  while(start <= 5280) {
+    console.log(start);
+    start++;
+  }
+}
+
+integersWhile();
